ad89c811ce80c92ff61b132af91dc344c245942a
FredieDidier <fdidier@terra.com.br>
2024-11-18 14:44:56 +0000

    Analysis - Adding pop analysis

--- analysis/code/_table_reg_pop.R
+++ analysis/code/_table_reg_pop.R
@@ -137,3 +137,382 @@ regression_table = etable(reg1, reg2, reg3, reg4, reg5, reg6,
                           title = "Impact of Policy on Homicide Rates by Population Size",
                           dict = dict,
                           file = paste0(GITHUB_PATH, "analysis/output/tables/pop_size.tex"))
+
+
+
+############################################
+
+# Robustness Analysis (Distance to Treated Border)
+
+############################################
+
+# Criar as três subamostras baseadas na distância e criar variável neighbor
+main_data_100km <- main_data %>% 
+  filter(dist_treated < 100) %>%
+  rename(neighbor = treated,
+         neighbor_population = treated_population,
+         neighbor_population_median = treated_population_median,
+         neighbor_log_population = treated_log_population,
+         neighbor_population_2000 = treated_population_2000,
+         neighbor_population_2000_median = treated_population_2000_median,
+         neighbor_log_population_2000 = treated_log_population_2000)
+
+main_data_75km <- main_data %>% 
+  filter(dist_treated < 75) %>%
+  rename(neighbor = treated,
+         neighbor_population = treated_population,
+         neighbor_population_median = treated_population_median,
+         neighbor_log_population = treated_log_population,
+         neighbor_population_2000 = treated_population_2000,
+         neighbor_population_2000_median = treated_population_2000_median,
+         neighbor_log_population_2000 = treated_log_population_2000)
+
+main_data_50km <- main_data %>% 
+  filter(dist_treated < 50) %>%
+  rename(neighbor = treated,
+         neighbor_population = treated_population,
+         neighbor_population_median = treated_population_median,
+         neighbor_log_population = treated_log_population,
+         neighbor_population_2000 = treated_population_2000,
+         neighbor_population_2000_median = treated_population_2000_median,
+         neighbor_log_population_2000 = treated_log_population_2000)
+
+# Função para rodar todas as regressões em uma subamostra
+run_all_regressions <- function(data) {
+  # Taxa de homicídio em nível
+  reg1 <- feols(taxa_homicidios_total_por_100mil_munic ~ neighbor + neighbor_population | 
+                  municipality_code + year,
+                data = data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg2 <- feols(log_tx_homicidio ~ neighbor + neighbor_population | 
+                  municipality_code + year,
+                data = data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg3 <- feols(taxa_homicidios_total_por_100mil_munic ~ neighbor + neighbor_population_median | 
+                  municipality_code + year,
+                data = data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg4 <- feols(log_tx_homicidio ~ neighbor + neighbor_population_median | 
+                  municipality_code + year,
+                data = data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg5 <- feols(taxa_homicidios_total_por_100mil_munic ~ neighbor + neighbor_log_population | 
+                  municipality_code + year,
+                data = data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg6 <- feols(log_tx_homicidio ~ neighbor + neighbor_log_population | 
+                  municipality_code + year,
+                data = data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg7 <- feols(taxa_homicidios_total_por_100mil_munic ~ neighbor + neighbor_population_2000 | 
+                  municipality_code + year,
+                data = data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg8 <- feols(log_tx_homicidio ~ neighbor + neighbor_population_2000 | 
+                  municipality_code + year,
+                data = data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg9 <- feols(taxa_homicidios_total_por_100mil_munic ~ neighbor + neighbor_population_2000_median | 
+                  municipality_code + year,
+                data = data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg10 <- feols(log_tx_homicidio ~ neighbor + neighbor_population_2000_median | 
+                   municipality_code + year,
+                 data = data,
+                 weights = ~ population_2000_muni,
+                 cluster = c("state_code"))
+  
+  reg11 <- feols(taxa_homicidios_total_por_100mil_munic ~ neighbor + neighbor_log_population_2000 | 
+                   municipality_code + year,
+                 data = data,
+                 weights = ~ population_2000_muni,
+                 cluster = c("state_code"))
+  
+  reg12 <- feols(log_tx_homicidio ~ neighbor + neighbor_log_population_2000 | 
+                   municipality_code + year,
+                 data = data,
+                 weights = ~ population_2000_muni,
+                 cluster = c("state_code"))
+  
+  return(list(reg1, reg2, reg3, reg4, reg5, reg6,
+              reg7, reg8, reg9, reg10, reg11, reg12))
+}
+
+# Rodar regressões para cada subamostra
+regs_100km <- run_all_regressions(main_data_100km)
+regs_75km <- run_all_regressions(main_data_75km)
+regs_50km <- run_all_regressions(main_data_50km)
+
+# Criar dicionário atualizado com os novos nomes
+dict <- c("taxa_homicidios_total_por_100mil_munic" = "Homicide Rate",
+          "log_tx_homicidio" = "Log(Homicide Rate + 1)",
+          "neighbor" = "Neighbor",
+          "neighbor_population" = "Neighbor × Pop",
+          "neighbor_population_median" = "Neighbor × Pop > Median",
+          "neighbor_log_population" = "Neighbor × Log(Pop)",
+          "neighbor_population_2000" = "Neighbor × Pop 2000",
+          "neighbor_population_2000_median" = "Neighbor × Pop 2000 > Median",
+          "neighbor_log_population_2000" = "Neighbor × Log(Pop 2000)",
+          "municipality_code" = "Municipality",
+          "year" = "Year")
+
+# Criar uma única tabela com todas as regressões
+regression_table = etable(
+  # 100km
+  regs_100km[[1]], regs_100km[[2]], 
+  regs_100km[[3]], regs_100km[[4]], 
+  regs_100km[[5]], regs_100km[[6]],
+  regs_100km[[7]], regs_100km[[8]], 
+  regs_100km[[9]], regs_100km[[10]], 
+  regs_100km[[11]], regs_100km[[12]],
+  # 75km
+  regs_75km[[1]], regs_75km[[2]], 
+  regs_75km[[3]], regs_75km[[4]], 
+  regs_75km[[5]], regs_75km[[6]],
+  regs_75km[[7]], regs_75km[[8]], 
+  regs_75km[[9]], regs_75km[[10]], 
+  regs_75km[[11]], regs_75km[[12]],
+  # 50km
+  regs_50km[[1]], regs_50km[[2]], 
+  regs_50km[[3]], regs_50km[[4]], 
+  regs_50km[[5]], regs_50km[[6]],
+  regs_50km[[7]], regs_50km[[8]], 
+  regs_50km[[9]], regs_50km[[10]], 
+  regs_50km[[11]], regs_50km[[12]],
+  cluster = ~ state_code,
+  signif.code = c("***" = 0.01, "**" = 0.05, "*" = 0.1),
+  digits = 4,
+  fixef_sizes = TRUE,
+  title = "Impact of Policy on Homicide Rates by Distance from Border",
+  dict = dict,
+  headers = c(rep("100km", 12), rep("75km", 12), rep("50km", 12)),
+  file = paste0(GITHUB_PATH, "analysis/output/tables/border_analysis.tex")
+)
+
+
+#################################
+#.   SPILLOVER ANALYSIS
+##################################
+
+# Create spillover variables for different distance thresholds
+distance_thresholds <- c(50, 75, 100)
+
+# Filtrar apenas estados não tratados
+main_data <- main_data[!state %in% c("PE", "BA", "PB", "CE", "MA")]
+
+for(dist in distance_thresholds) {
+
+  # Initialize spillover variable
+  main_data[, paste0("spillover_", dist) := 0]
+  
+  # 2007-2010: Only PE spillovers
+  main_data[year >= 2007 & year <= 2010 & 
+              !state %in% c("PE", "BA", "PB", "CE", "MA") & 
+              dist_PE <= dist, 
+            paste0("spillover_", dist) := 1]
+  
+  # 2011-2014: PE, BA, PB spillovers (take minimum distance)
+  main_data[year >= 2011 & year <= 2014 & 
+              !state %in% c("PE", "BA", "PB", "CE", "MA") & 
+              pmin(dist_PE, dist_BA, dist_PB) <= dist, 
+            paste0("spillover_", dist) := 1]
+  
+  # 2015: PE, BA, PB, CE spillovers
+  main_data[year == 2015 & 
+              !state %in% c("PE", "BA", "PB", "CE", "MA") & 
+              pmin(dist_PE, dist_BA, dist_PB, dist_CE) <= dist, 
+            paste0("spillover_", dist) := 1]
+  
+  # 2016 onwards: All treated states spillovers
+  main_data[year >= 2016 & 
+              !state %in% c("PE", "BA", "PB", "CE", "MA") & 
+              pmin(dist_PE, dist_BA, dist_PB, dist_CE, dist_MA) <= dist, 
+            paste0("spillover_", dist) := 1]
+  
+  # Create population interaction terms (current and 2000)
+  main_data[, paste0("spillover_", dist, "_population") := 
+              get(paste0("spillover_", dist)) * population_muni]
+  main_data[, paste0("spillover_", dist, "_population_median") := 
+              get(paste0("spillover_", dist)) * population_median]
+  main_data[, paste0("spillover_", dist, "_log_population") := 
+              get(paste0("spillover_", dist)) * log_population_muni]
+  
+  # 2000 population interactions
+  main_data[, paste0("spillover_", dist, "_population_2000") := 
+              get(paste0("spillover_", dist)) * population_2000_muni]
+  main_data[, paste0("spillover_", dist, "_population_2000_median") := 
+              get(paste0("spillover_", dist)) * population_2000_median]
+  main_data[, paste0("spillover_", dist, "_log_population_2000") := 
+              get(paste0("spillover_", dist)) * log_population_2000_muni]
+}
+
+# Function to run regressions for each distance and specification
+run_spillover_regressions <- function(dist) {
+  # Linear specifications
+  reg1 <- feols(taxa_homicidios_total_por_100mil_munic ~ 
+                  get(paste0("spillover_", dist)) + 
+                  get(paste0("spillover_", dist, "_population")) | 
+                  municipality_code + year,
+                data = main_data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg2 <- feols(log_tx_homicidio ~ 
+                  get(paste0("spillover_", dist)) + 
+                  get(paste0("spillover_", dist, "_population")) | 
+                  municipality_code + year,
+                data = main_data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  # Median specifications
+  reg3 <- feols(taxa_homicidios_total_por_100mil_munic ~ 
+                  get(paste0("spillover_", dist)) + 
+                  get(paste0("spillover_", dist, "_population_median")) | 
+                  municipality_code + year,
+                data = main_data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg4 <- feols(log_tx_homicidio ~ 
+                  get(paste0("spillover_", dist)) + 
+                  get(paste0("spillover_", dist, "_population_median")) | 
+                  municipality_code + year,
+                data = main_data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  # Log specifications
+  reg5 <- feols(taxa_homicidios_total_por_100mil_munic ~ 
+                  get(paste0("spillover_", dist)) + 
+                  get(paste0("spillover_", dist, "_log_population")) | 
+                  municipality_code + year,
+                data = main_data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg6 <- feols(log_tx_homicidio ~ 
+                  get(paste0("spillover_", dist)) + 
+                  get(paste0("spillover_", dist, "_log_population")) | 
+                  municipality_code + year,
+                data = main_data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  # 2000 population specifications
+  reg7 <- feols(taxa_homicidios_total_por_100mil_munic ~ 
+                  get(paste0("spillover_", dist)) + 
+                  get(paste0("spillover_", dist, "_population_2000")) | 
+                  municipality_code + year,
+                data = main_data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg8 <- feols(log_tx_homicidio ~ 
+                  get(paste0("spillover_", dist)) + 
+                  get(paste0("spillover_", dist, "_population_2000")) | 
+                  municipality_code + year,
+                data = main_data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg9 <- feols(taxa_homicidios_total_por_100mil_munic ~ 
+                  get(paste0("spillover_", dist)) + 
+                  get(paste0("spillover_", dist, "_population_2000_median")) | 
+                  municipality_code + year,
+                data = main_data,
+                weights = ~ population_2000_muni,
+                cluster = c("state_code"))
+  
+  reg10 <- feols(log_tx_homicidio ~ 
+                   get(paste0("spillover_", dist)) + 
+                   get(paste0("spillover_", dist, "_population_2000_median")) | 
+                   municipality_code + year,
+                 data = main_data,
+                 weights = ~ population_2000_muni,
+                 cluster = c("state_code"))
+  
+  reg11 <- feols(taxa_homicidios_total_por_100mil_munic ~ 
+                   get(paste0("spillover_", dist)) + 
+                   get(paste0("spillover_", dist, "_log_population_2000")) | 
+                   municipality_code + year,
+                 data = main_data,
+                 weights = ~ population_2000_muni,
+                 cluster = c("state_code"))
+  
+  reg12 <- feols(log_tx_homicidio ~ 
+                   get(paste0("spillover_", dist)) + 
+                   get(paste0("spillover_", dist, "_log_population_2000")) | 
+                   municipality_code + year,
+                 data = main_data,
+                 weights = ~ population_2000_muni,
+                 cluster = c("state_code"))
+  
+  return(list(reg1, reg2, reg3, reg4, reg5, reg6, reg7, reg8, reg9, reg10, reg11, reg12))
+}
+
+# Run all regressions and create tables
+all_regressions <- list()
+for (dist in distance_thresholds) {
+  all_regressions[[as.character(dist)]] <- run_spillover_regressions(dist)
+}
+
+dict_spillover <- c(
+  # [Previous dictionary entries remain the same]
+  "spillover_50_population_2000" = "Spillover (50km) × Pop 2000",
+  "spillover_75_population_2000" = "Spillover (75km) × Pop 2000",
+  "spillover_100_population_2000" = "Spillover (100km) × Pop 2000",
+  "spillover_50_population_2000_median" = "Spillover (50km) × Pop 2000 > Median",
+  "spillover_75_population_2000_median" = "Spillover (75km) × Pop 2000 > Median",
+  "spillover_100_population_2000_median" = "Spillover (100km) × Pop 2000 > Median",
+  "spillover_50_log_population_2000" = "Spillover (50km) × Log(Pop 2000)",
+  "spillover_75_log_population_2000" = "Spillover (75km) × Log(Pop 2000)",
+  "spillover_100_log_population_2000" = "Spillover (100km) × Log(Pop 2000)"
+)
+# Create single regression table with all specifications
+regression_table = etable(
+  # 50km specifications
+  all_regressions[["50"]][[1]], all_regressions[["50"]][[2]], 
+  all_regressions[["50"]][[3]], all_regressions[["50"]][[4]],
+  all_regressions[["50"]][[5]], all_regressions[["50"]][[6]],
+  all_regressions[["50"]][[7]], all_regressions[["50"]][[8]],
+  all_regressions[["50"]][[9]], all_regressions[["50"]][[10]], 
+  all_regressions[["50"]][[11]], all_regressions[["50"]][[12]],
+  # 75km specifications  
+  all_regressions[["75"]][[1]], all_regressions[["75"]][[2]],
+  all_regressions[["75"]][[3]], all_regressions[["75"]][[4]],
+  all_regressions[["75"]][[5]], all_regressions[["75"]][[6]],
+  all_regressions[["75"]][[7]], all_regressions[["75"]][[8]],
+  all_regressions[["75"]][[9]], all_regressions[["75"]][[10]],
+  all_regressions[["75"]][[11]], all_regressions[["75"]][[12]],
+  # 100km specifications
+  all_regressions[["100"]][[1]], all_regressions[["100"]][[2]],
+  all_regressions[["100"]][[3]], all_regressions[["100"]][[4]], 
+  all_regressions[["100"]][[5]], all_regressions[["100"]][[6]],
+  all_regressions[["100"]][[7]], all_regressions[["100"]][[8]],
+  all_regressions[["100"]][[9]], all_regressions[["100"]][[10]],
+  all_regressions[["100"]][[11]], all_regressions[["100"]][[12]],
+  cluster = ~ state_code + municipality_code,
+  signif.code = c("***" = 0.01, "**" = 0.05, "*" = 0.1),
+  digits = 4,
+  fixef_sizes = TRUE,
+  title = "Spillover Effects by Distance and Population",
+  dict = dict_spillover,
+  file = paste0(GITHUB_PATH, "analysis/output/tables/spillover_all.tex"))
--- build/area/code/area.R
+++ build/area/code/area.R
@@ -1,19 +1,21 @@
 # I download municipal area information of 2010 from IBGE website: https://www.ibge.gov.br/geociencias/organizacao-do-territorio/malhas-territoriais/15774-malhas.html?edicao=27421
 
 library(sf)
-library(purrr)
 library(dplyr)
+library(purrr)
 library(janitor)
 library(units)
 
-# Define the dictionary for UF codes and abbreviations
+# Define o dicionário para códigos UF e abreviações
 uf_dict <- c("21" = "MA", "22" = "PI", "23" = "CE", 
-             "24" = "RN", "25" = "PB", "26" = "PE", "27" = "AL", "28" = "SE", 
-             "29" = "BA")
+             "24" = "RN", "25" = "PB", "26" = "PE", 
+             "27" = "AL", "28" = "SE", "29" = "BA")
 
-# Function to read a single shapefile
+# Função para ler um único shapefile
 read_uf_shapefile <- function(uf_code, uf_abbr) {
-  file_path <- file.path(paste0(DROPBOX_PATH, "build/area/input"), uf_abbr, paste0(uf_code, "MUE250GC_SIR.shp"))
+  file_path <- file.path(paste0(DROPBOX_PATH, "build/area/input"), 
+                         uf_abbr, 
+                         paste0(uf_code, "MUE250GC_SIR.shp"))
   tryCatch({
     sf::read_sf(file_path) %>%
       mutate(UF = uf_abbr)
@@ -23,39 +25,90 @@ read_uf_shapefile <- function(uf_code, uf_abbr) {
   })
 }
 
-# Read all shapefiles and combine them
+# Ler todos os shapefiles e combiná-los
 area <- uf_dict %>%
   imap_dfr(~ read_uf_shapefile(uf_code = .y, uf_abbr = .x))
 
-# Cleaning names and selecting columns
-area = area %>% 
-  clean_names()
-
-area$nm_municip = NULL
-area$id = NULL
-area$UF = NULL
-
-area = area %>% 
+# Limpar nomes e selecionar colunas
+area <- area %>% 
+  clean_names() %>%
+  select(cd_geocodm) %>%
   rename(municipality_code = cd_geocodm)
 
-# Extracting area in km^2 information
-area = area %>%
-  mutate(area_km2 = as.numeric(units::set_units(st_area(.), km^2)))
+# Converter código do município para inteiro
+area$municipality_code <- as.integer(area$municipality_code)
 
-area$municipality_code = as.integer(area$municipality_code)
+# Calculando área, longitude e latitude
+area <- area %>%
+  mutate(
+    area_km2 = as.numeric(units::set_units(st_area(.), km^2))
+  )
 
-# Extraindo o centroide do polígono
+# Extraindo centroides para lat/long
 centroides <- st_centroid(area)
-
-# Extraindo as coordenadas de latitude e longitude do centroide
 coords_centroides <- st_coordinates(centroides)
 
-# Adicionando as colunas de latitude e longitude à base
+# Adicionando lat/long à base
 area <- area %>%
-  mutate(longitude = coords_centroides[, 1],  # Longitude é a 1ª coluna
-         latitude = coords_centroides[, 2])   # Latitude é a 2ª coluna
+  mutate(
+    longitude = coords_centroides[, 1],
+    latitude = coords_centroides[, 2]
+  )
 
-# Removing geometry column
+# Ler a base de estados
+s <- st_read(paste0(DROPBOX_PATH,"build/area/input/BR_UF_2022/BR_UF_2022.shp"))
+
+# Verificar e padronizar CRS
+if (st_crs(area) != st_crs(s)) {
+  area <- st_transform(area, st_crs(s))
+}
+
+# Criar o polígono dos estados tratados
+treated_states <- s %>%
+  filter(CD_UF %in% c("21", "23", "25", "26", "29")) %>%
+  st_union() %>%
+  st_make_valid()
+
+# Criar o contorno (borda) dos estados tratados
+treated_border <- st_boundary(treated_states)
+
+# Função para calcular distância para cada estado
+calculate_state_distance <- function(data, state_data, cd_uf) {
+  state_border <- state_data %>%
+    filter(CD_UF == cd_uf) %>%
+    st_union() %>%
+    st_make_valid() %>%
+    st_boundary()
+  
+  data %>%
+    mutate(
+      centroid = st_centroid(geometry),
+      !!paste0("dist_", uf_dict[cd_uf]) := as.numeric(st_distance(centroid, state_border)) / 1000,
+      centroid = NULL
+    )
+}
+
+# Calcular a distância inicial aos estados tratados
+area_final <- area %>%
+  mutate(
+    centroid = st_centroid(geometry),
+    dist_treated = as.numeric(st_distance(centroid, treated_border)) / 1000,
+    centroid = NULL
+  )
+
+# Calcular distância para cada estado individual
+area_final <- area_final %>%
+  calculate_state_distance(s, "26") %>%  # PE
+  calculate_state_distance(s, "29") %>%  # BA
+  calculate_state_distance(s, "25") %>%  # PB
+  calculate_state_distance(s, "23") %>%  # CE
+  calculate_state_distance(s, "21")      # MA
+
+# Selecionar e ordenar as colunas finais
+area_final <- area_final %>%
+  select(municipality_code, geometry, area_km2, longitude, latitude, 
+         dist_treated, dist_PE, dist_BA, dist_PB, dist_CE, dist_MA)
+area = area_final
 area = st_drop_geometry(area)
 
 # Saving clean dataset
